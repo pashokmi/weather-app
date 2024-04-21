52b9d20c17f33238b56177f3430a02a8a920cdcf
pashokmi <pashokmi@gmail.com>
2024-04-21 14:08:44 +0300

    test build

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ const App = () => {
             dispatch(addUser(userData));
         }
         console.log('треба знати як часто юзер стрибає ')
-    }, [user])
+    }, [dispatch, user])
 
 
     const handleLogout = () => {
